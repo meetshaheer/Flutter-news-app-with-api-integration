098727dc5192d22a249fbf9a3af0ed48c5dcfd16
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-09-26 14:39:38 +0500

    fetch data from api and show it in main screen listview banner

--- flutter_application_1/lib/News_Apis.dart/apple_api.dart
+++ flutter_application_1/lib/News_Apis.dart/apple_api.dart
@@ -3,7 +3,7 @@ import 'package:http/http.dart' as http;
 import 'package:flutter_application_1/Models/news_model.dart';
 
 List<Article> appleData = [];
-
+String placeholder = "https://knetic.org.uk/wp-content/uploads/2020/07/Pcture-Placeholder.png";
 Future<List<Article>> getAppleNews() async {
   var url = Uri.parse(
       "https://newsapi.org/v2/everything?q=apple&from=2024-09-24&to=2024-09-24&sortBy=popularity&apiKey=a68ea7f819eb47d093b67111b570b654");
@@ -14,7 +14,7 @@ Future<List<Article>> getAppleNews() async {
   var filterrespose = responsebody['articles'];
 
   for (var eachMap in filterrespose) {
-    if (eachMap['title'] != "[Removed]") {
+    if (eachMap['title'] != "[Removed]" && eachMap['urlToImage'] != null) {
       appleData.add(Article.fromJson(eachMap));
     }
   }
