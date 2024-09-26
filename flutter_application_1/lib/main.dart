e090e81e1f4f7da424efed5708cd3de13ad5e3d2
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-09-26 11:04:54 +0500

    Ftech the data from API and test it.

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/Models/news_model.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 
@@ -11,7 +12,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: newsAPI(),
     );
   }
@@ -24,9 +25,31 @@ class newsAPI extends StatefulWidget {
   State<newsAPI> createState() => _newsAPIState();
 }
 
+List<Article> resposeData = [];
+
+getNewsAPI() async {
+  var url = Uri.parse(
+      "https://newsapi.org/v2/everything?q=apple&from=2024-09-24&to=2024-09-24&sortBy=popularity&apiKey=a68ea7f819eb47d093b67111b570b654");
+
+  var response = await http.get(url);
+  var responsebody = jsonDecode(response.body);
+
+  var filterrespose = responsebody['articles'];
+  print(filterrespose);
+}
+
 class _newsAPIState extends State<newsAPI> {
   @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getNewsAPI();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      
+    );
   }
 }
