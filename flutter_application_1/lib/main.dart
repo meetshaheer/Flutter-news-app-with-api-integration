7e1d7a8f1458f283b1c61f041db8143dae1eeacb
Shaheer Ahmed <shaheer.ahmed0404@gmail.com>
2024-09-28 19:28:13 +0500

    updated

--- flutter_application_1/lib/main.dart
+++ flutter_application_1/lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:UKBD/Models/news_model.dart';
-import 'package:UKBD/screens/onboarding_screen.dart';
 import 'package:flutter/material.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
